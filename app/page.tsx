62a84d7886b6e73d32ae349b25ac0f3239cb6657
reedwi <riandolo103@gmail.com>
2023-11-17 17:38:44 -0500

    taking away experience for now

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import About from "@/components/about";
-import Experience from "@/components/experience";
+// import Experience from "@/components/experience";
 import Intro from "@/components/intro";
 import Projects from "@/components/projects";
 import SectionDivider from "@/components/section-divider";
